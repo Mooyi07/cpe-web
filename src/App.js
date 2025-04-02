1fbb0251688ed6fd78fbeb4ac4448d60090bbd89
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-02 12:09:13 +0800

    Initial launch of cpe web

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
       </header>
       <div className="display-form">
         <h1>Student Access Module</h1>
+        <form>
+          <h1>User Authentication</h1>
+          <label for="username">Username :</label><br></br>
+          <input type="text" id="username" name="username" placeholder="Username"></input><br></br>
+          <label for="password">Password :</label><br></br>
+          <input type="password" id="password" name="password" placeholder="Password"></input><br></br>
+          <label for="birthday">Birthday :</label><br></br>
+          <input type="date" id="birthday" name="birthday" placeholder="Birth Date"></input><br></br>
+        </form>
       </div>
     </div>
   );
