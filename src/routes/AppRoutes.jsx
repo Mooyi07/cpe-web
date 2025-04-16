a48d9f00978618f745e9d7d01641271dabd58f31
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-16 10:44:17 +0800

    Added Route in react

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import Grades from "../pages/Grades";
+import Schedule from "../pages/Schedule";
+import Layout from "../components/Layout";  // import Layout
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      
+      {/* Wrap all other routes with Layout */}
+      <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+      <Route path="/grades" element={<Layout><Grades /></Layout>} />
+      <Route path="/schedule" element={<Layout><Schedule /></Layout>} />
+      {/* Add more routes as needed */}
+    </Routes>
+  );
+};
+
+export default AppRoutes;
