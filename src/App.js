916c1f91e611b01c3eab52b6a0c95ba2ca5e9976
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-16 10:44:36 +0800

    Changed the App.js to set up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import logo from './logo.png';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login'; // Adjust path based on your folder structure
+import Dashboard from './pages/Dashboard'; // Example route
 
 function App() {
   return (
-    <div className="App">
-      <header className="brand">
-        <img src={logo} className="brand-logo" alt="logo"></img>
-        <h1 className="brand-name">Computer Department</h1>
-      </header>
-      <div className="display-form">
-        <h1>Student Access Module</h1>
-        <form method='GET'>
-          <h1>User Authentication</h1>
-          <label for="username">Username :</label>
-          <input type="text" id="username" name="username" placeholder="Username"></input>
-          <label for="password">Password :</label>
-          <input type="password" id="password" name="password" placeholder="Password"></input>
-          <label for="birthday">Birthday :</label>
-          <input type="date" id="birthday" name="birthday" placeholder="Birth Date"></input>
-
-          <div className="button-container">
-            <button type="reset">Clear Entries</button>
-            <button type="submit">Login</button>
-          </div>
-
-          <p>Forgot you password? Click Here</p>
-        </form>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
