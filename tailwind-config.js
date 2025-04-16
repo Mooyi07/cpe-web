3ff1b399c6f1ce64e858ce06af2c91748dcd4b4a
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-16 10:43:52 +0800

    Tailwind Configuration

---
+++ tailwind-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",  // Ensures Tailwind scans React files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
