f8ac2a7c2ad349a431e2a0938e65013f37816724
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-23 14:48:38 +0800

    Seperated the scheduleData

---
+++ src/data/scheduleData.js
@@ -0,0 +1,20 @@
+const schedule = [
+    { day: "Monday", subject_code: "CpE 223", subject: "Computer Programming 5", prof: "Quintanilla", room: "ECL", time: "8:15 AM - 12:00 PM" },
+    { day: "Monday", subject_code: "MATH 332", subject: "Numerical Methods", prof: "Romero", room: "MTB R1", time: "1:00 PM - 6:00 PM" },
+    { day: "Tuesday", subject_code: "CpE 234A", subject: "Logic Circuits and Design", prof: "Lumauag", room: "MTB R1", time: "8:15 AM - 12:00 PM" },
+    { day: "Tuesday", subject_code: "CpE 232", subject: "Computer Engineering Drafting and Design", prof: "Padernal", room: "R 19", time: "1:00 PM - 6:00 PM" },
+    { day: "Wednesday", subject_code: "CpE 234", subject: "Software Design", prof: "Apawan", room: "R 19", time: "8:15 AM - 12:00 PM" },
+    { day: "Wednesday", subject_code: "CpE 234A", subject: "Logic Circuits and Design", prof: "Lumauag", room: "R 19", time: "1:00 PM - 4:45 PM" },
+    { day: "Thursday", subject_code: "BES 232", subject: "Dynamics of Rigid Bodies", prof: "Bergola", room: "R 19", time: "9:30 AM - 12:00 PM" },
+    { day: "Thursday", subject_code: "GEC 6", subject: "Art Appreciation", prof: "Discutido", room: "R 19", time: "1:00 PM - 4:45 PM" },
+    { day: "Friday", subject_code: "CpE 234", subject: "Software Design", prof: "Apawan", room: "ECL", time: "8:15 AM - 12:00 PM" },
+    { day: "Friday", subject_code: "CpE 223", subject: "Computer Programming 5", prof: "Quintanilla", room: "ECL", time: "1:00 PM - 3:30 PM" },
+    { day: "Monday", subject: "Lunch Break", time: "12:00 PM - 1:00 PM" },
+    { day: "Tuesday", subject: "Lunch Break", time: "12:00 PM - 1:00 PM" },
+    { day: "Wednesday", subject: "Lunch Break", time: "12:00 PM - 1:00 PM" },
+    { day: "Thursday", subject: "Lunch Break", time: "12:00 PM - 1:00 PM" },
+    { day: "Friday", subject: "Lunch Break", time: "12:00 PM - 1:00 PM" },
+  ];
+  
+  export default schedule;
+  
