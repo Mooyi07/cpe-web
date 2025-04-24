46b3722a49d4c93aa0a77e535415ca66f413328a
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-24 16:06:53 +0800

    Software Design

--- src/components/GradesTable.jsx
+++ src/components/GradesTable.jsx
@@ -4,7 +4,7 @@ const grades = [
   { subCode: 'CPE233-V', name: 'Computer Programming 5', prelim: 6, midterm: 7, endterm: 6.5 },
   { subCode: 'CPE232-V', name: 'Computer Engineering Drafting and Design', prelim: 6.5, midterm: 4.0, endterm: 5.5 },
   { subCode: 'BES223-V', name: 'Statics of Rigid Bodies', prelim: 6, midterm: 7, endterm: 6.5 },
-  { subCode: 'CPE234-V', name: 'Software Design', prelim: 2.8, midterm: 4.0, endterm: 6.9 },
+  { subCode: 'CPE234-V', name: 'Software Design', prelim: 4.5, midterm: 7.4, endterm: 7.3 },
   { subCode: 'CPE234A-V', name: 'Logic Circuits and Design', prelim: 5.5, midterm: 4.2, endterm: 5 },
   { subCode: 'MATH333-V', name: 'Numerical Methods', prelim: 7, midterm: 7, endterm: 3.8 },
 ];
