8b717edd44cf4520f7d7518bd411fd02c35a73c9
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-24 16:07:43 +0800

    Computer Engineering Drafting Design changes

--- src/components/GradesTable.jsx
+++ src/components/GradesTable.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const grades = [
   { subCode: 'CPE233-V', name: 'Computer Programming 5', prelim: 6, midterm: 7, endterm: 6.5 },
-  { subCode: 'CPE232-V', name: 'Computer Engineering Drafting and Design', prelim: 6.5, midterm: 4.0, endterm: 5.5 },
+  { subCode: 'CPE232-V', name: 'Computer Engineering Drafting and Design', prelim: 6.8, midterm: 5.7, endterm: 6.0 },
   { subCode: 'BES223-V', name: 'Statics of Rigid Bodies', prelim: 6, midterm: 7, endterm: 6.5 },
   { subCode: 'CPE234-V', name: 'Software Design', prelim: 4.5, midterm: 7.4, endterm: 7.3 },
   { subCode: 'CPE234A-V', name: 'Logic Circuits and Design', prelim: 5.5, midterm: 4.2, endterm: 5 },
