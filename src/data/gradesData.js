1548dafc80b66d22b84f4e28a27a1916bca04c29
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-28 11:01:16 +0800

    Seperated dummy data

---
+++ src/data/gradesData.js
@@ -0,0 +1,13 @@
+// gradesData.js
+
+const grades = [
+    { subCode: 'CPE233-V', name: 'Computer Programming 5', prelim: 5.9, midterm: 4.9, endterm: 6.6 },
+    { subCode: 'CPE232-V', name: 'Computer Engineering Drafting and Design', prelim: 6.8, midterm: 5.7, endterm: 6.0 },
+    { subCode: 'BES223-V', name: 'Statics of Rigid Bodies', prelim: 6, midterm: 7, endterm: 6.5 },
+    { subCode: 'CPE234-V', name: 'Software Design', prelim: 4.5, midterm: 7.4, endterm: 7.3 },
+    { subCode: 'CPE234A-V', name: 'Logic Circuits and Design', prelim: 5.5, midterm: 4.2, endterm: 5 },
+    { subCode: 'MATH333-V', name: 'Numerical Methods', prelim: 7, midterm: 7, endterm: 3.8 },
+  ];
+  
+  export default grades;
+  
