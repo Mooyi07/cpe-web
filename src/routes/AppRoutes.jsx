19009568bb4ec341237759a033294e4d37314f86
Mooyi07 <ljt.villanueva@gmail.com>
2025-05-01 12:51:51 +0800

    Added 404 not Found Page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,21 +7,23 @@ import Announcement from "../pages/Announcement";
 import Profile from "../pages/Profile";     
 import Settings from "../pages/Settings";     
 import Layout from "../components/Layout";   
+import NotFound from "../pages/NotFound"; 
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-
-      {/* Protected Routes with Layout */}
       <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
       <Route path="/grades" element={<Layout><Grades /></Layout>} />
       <Route path="/schedule" element={<Layout><Schedule /></Layout>} />
       <Route path="/announcement" element={<Layout><Announcement /></Layout>} />
-      <Route path="/profile" element={<Layout><Profile /></Layout>} />     
-      <Route path="/settings" element={<Layout><Settings /></Layout>} /> 
+      <Route path="/profile" element={<Layout><Profile /></Layout>} />
+      <Route path="/settings" element={<Layout><Settings /></Layout>} />
+      
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
+
 
 export default AppRoutes;
